6965bdc39b2252584640e63c7e231058a8aa86b0
apauliuc <andupauliuc@gmail.com>
2018-10-04 00:09:25 +0200

    Full size visual features to context avaiable

--- lstm.py
+++ lstm.py
@@ -10,7 +10,7 @@ class LSTM(torch.nn.Module):
 
     def __init__(self, vocab_size, output_size, embedding_size, hidden_size, img_feature_size=2048,
                  number_stacked_lstms=1, visual_model=False, pretrained_embeddings=None, embedding_trainable=True,
-                 visual_features_location=None, dropout=0):
+                 visual_features_location=None, dropout=0, full_size_visual_features=False):
         """
 
         :param vocab_size:
@@ -30,7 +30,8 @@ class LSTM(torch.nn.Module):
 
         self.visual_features_location = visual_features_location
         self.visual_model = visual_model
-        self.hidden_size = hidden_size
+        self.hidden_size = hidden_size if full_size_visual_features is False else img_feature_size
+        self.full_size_visual_features = full_size_visual_features
         self.number_stacked_lstms = number_stacked_lstms
 
         self.embedding = torch.torch.nn.Embedding(vocab_size, embedding_size)
@@ -40,21 +41,21 @@ class LSTM(torch.nn.Module):
                 self.embedding.weight.requires_grad = False
 
         if self.visual_model and 'lstm_input' in self.visual_features_location:
-            self.lstm = torch.nn.LSTM(input_size=embedding_size + img_feature_size, hidden_size=hidden_size,
+            self.lstm = torch.nn.LSTM(input_size=embedding_size + img_feature_size, hidden_size=self.hidden_size,
                                       num_layers=number_stacked_lstms,
                                       batch_first=True, dropout=dropout)
         else:
-            self.lstm = torch.nn.LSTM(input_size=embedding_size, hidden_size=hidden_size,
+            self.lstm = torch.nn.LSTM(input_size=embedding_size, hidden_size=self.hidden_size,
                                       num_layers=number_stacked_lstms,
                                       batch_first=True, dropout=dropout)
-        if self.visual_model and 'lstm_context' in self.visual_features_location:
+        if self.visual_model and 'lstm_context' in self.visual_features_location and not full_size_visual_features:
             self.visual_features_to_layer_features = torch.nn.Linear(img_feature_size, img_feature_size)
-            self.layer_features_to_hidden = torch.nn.Linear(img_feature_size, hidden_size)
+            self.layer_features_to_hidden = torch.nn.Linear(img_feature_size, self.hidden_size)
         if self.visual_model and 'lstm_output' in self.visual_features_location:
-            self.lstm_to_linear = torch.nn.Linear(hidden_size + img_feature_size, hidden_size + img_feature_size)
-            self.output_linear = torch.nn.Linear(hidden_size + img_feature_size, output_size)
+            self.lstm_to_linear = torch.nn.Linear(self.hidden_size + img_feature_size, self.hidden_size + img_feature_size)
+            self.output_linear = torch.nn.Linear(self.hidden_size + img_feature_size, output_size)
         else:
-            self.output_linear = torch.nn.Linear(hidden_size, img_feature_size)
+            self.output_linear = torch.nn.Linear(self.hidden_size, img_feature_size)
 
     def forward(self, sentence, image_features=None):
         input = self.embedding(sentence)
@@ -63,7 +64,9 @@ class LSTM(torch.nn.Module):
             # concat image to each input
             input = torch.cat((input, image_features.unsqueeze(1).repeat(1, input.shape[1], 1)), 2)
 
-        if self.visual_model and 'lstm_context' in self.visual_features_location:
+        if self.visual_model and self.full_size_visual_features:
+            c_0 = image_features.unsqueeze(0).repeat(self.number_stacked_lstms, 1, 1)
+        elif self.visual_model and 'lstm_context' in self.visual_features_location:
             layer_features = self.visual_features_to_layer_features(image_features)
             c_0 = self.layer_features_to_hidden(layer_features).unsqueeze(0).repeat(self.number_stacked_lstms, 1, 1)
         else:
